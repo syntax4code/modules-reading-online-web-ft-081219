86d10cc804f55172ea3f9cc4dbbce73b52488aec
Warren Harper II <warrenharper2@att.net>
2019-08-30 20:35:15 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,11 +1,11 @@
 module MetaDancing
- 
+
   def metadata
     "This class produces objects that love to dance."
-  end 
+  end
   class Dancer
   extend MetaDancing
-end 
+end
 class Kid
   extend MetaDancing
 end
